1bcba326efac3f27ccad7246d04cf8d35c9159c9
Iannealer <duwjdtn@ajou.ac.kr>
2021-12-27 21:33:44 +0900

    ResponseBody annotation

--- hello.spring/src/main/java/hello/hello/spring/controller/HelloController.java
+++ hello.spring/src/main/java/hello/hello/spring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hello.spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -12,5 +14,40 @@ public class HelloController {
         model.addAttribute("data", "hello!!");
         // 컨트롤러에서 문자 반환시 viewResolver가 화면을 찾아서 처리한다.
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template"
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody       // HTTP의 Body에 문자 내용을 반환.
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+
+        // 객체 반환시 스프링에서 HttpMessageConverter 내에 JSONConverter를 통해 변환 후 반환.
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
     }
 }
