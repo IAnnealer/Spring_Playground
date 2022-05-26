7d1b39aa4475f5f9f81ee203e2322927359141ca
Iannealer <duwjdtn@ajou.ac.kr>
2022-05-26 23:18:35 +0900

    Update DemoApplication

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,33 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
+@RestController						// makes this class to serve rest endpoints
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@GetMapping
+	public List<Student> hello() {
+		return List.of(
+				new Student(
+						1L,
+						"ian",
+						"duwjdtn1@gmail.com",
+						LocalDate.of(1994, Month.SEPTEMBER, 12),
+						29
+				)
+		);
+	}
 }
