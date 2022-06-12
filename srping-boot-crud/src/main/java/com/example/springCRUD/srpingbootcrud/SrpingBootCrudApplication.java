5cd0dbc2711206a265bcae1f87b7eb9c749831e9
Iannealer <duwjdtn@ajou.ac.kr>
2022-06-13 00:09:44 +0900

    CRUD Project init

---
+++ srping-boot-crud/src/main/java/com/example/springCRUD/srpingbootcrud/SrpingBootCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.springCRUD.srpingbootcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingBootCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrpingBootCrudApplication.class, args);
+	}
+
+}
---
+++ srping-boot-crud/src/test/java/com/example/springCRUD/srpingbootcrud/SrpingBootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springCRUD.srpingbootcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpingBootCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
